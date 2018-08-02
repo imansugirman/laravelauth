b1af47c499d56555857f8e90d27fc69887f8364f
imansugirman <iman.sugirman@icloud.com>
2018-08-02 20:20:02 +0700

    initial

---
+++ app/Exceptions/Handler.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Exceptions;
+
+use App\Mail\ExceptionOccured;
+use Illuminate\Support\Facades\Log;
+use Mail;
+use Symfony\Component\Debug\ExceptionHandler as SymfonyExceptionHandler;
+use Symfony\Component\Debug\Exception\FlattenException;
+use Exception;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+class Handler extends ExceptionHandler
+{
+    /**
+     * A list of the exception types that are not reported.
+     *
+     * @var array
+     */
+    protected $dontReport = [
+        //
+    ];
+
+    /**
+     * A list of the inputs that are never flashed for validation exceptions.
+     *
+     * @var array
+     */
+    protected $dontFlash = [
+        'password',
+        'password_confirmation',
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        // parent::report($exception);
+        $enableEmailExceptions = config('exceptions.emailExceptionEnabled');
+
+        if ($enableEmailExceptions === "") {
+            $enableEmailExceptions = config('exceptions.emailExceptionEnabledDefault');
+        }
+
+        if ($enableEmailExceptions && $this->shouldReport($exception)) {
+            $this->sendEmail($exception);
+        }
+
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Exception  $exception
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Exception $exception)
+    {
+        return parent::render($request, $exception);
+    }
+
+    /**
+     * Sends an email upon exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function sendEmail(Exception $exception)
+    {
+        try {
+
+            $e = FlattenException::create($exception);
+            $handler = new SymfonyExceptionHandler();
+            $html = $handler->getHtml($e);
+
+            Mail::send(new ExceptionOccured($html));
+
+        } catch (Exception $exception) {
+
+            Log::error($exception);
+
+        }
+    }
+}
